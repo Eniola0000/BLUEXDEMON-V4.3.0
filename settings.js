94fac142e7adfa8d937c3b76fb2b75a9c4788281
Eniola0000 <148587664+Eniola0000@users.noreply.github.com>
2025-01-01 23:34:37 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "2347037781124"
 
-global.botNumber = ["2349019529423"]
+global.botNumber = ["2347037781124"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
@@ -21,8 +21,8 @@ global.antiSpam = true
 global.sign = "☃️❄️"
 // ☞ ➻ ➽ ☛ 〆 
 global.bank = "Opay"
-global.bankname = "BLUE"
-global.accnumber = "7041039367"
+global.bankname = "AMINAT"
+global.accnumber = "7037781124"
 global.language = "en"
 global.sessionName = "session"
 global.pairingCode = true 
